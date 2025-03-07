cf9cfe3b7593791ddb8a3d38730b3506dd79a762
Syonn1 <118908098+Syonn1@users.noreply.github.com>
2025-03-07 09:23:32 +0700

    Update selenium_login_test.py

--- selenium_login_test.py
+++ selenium_login_test.py
@@ -11,8 +11,8 @@ def test_login_valid():
     password = driver.find_element(By.NAME, "password")
     submit_button = driver.find_element(By.NAME, "submit")
     
-    username.send_keys("valid_user")
-    password.send_keys("valid_password")
+    username.send_keys("irul")
+    password.send_keys("irul123")
     submit_button.click()
     
     time.sleep(2)
